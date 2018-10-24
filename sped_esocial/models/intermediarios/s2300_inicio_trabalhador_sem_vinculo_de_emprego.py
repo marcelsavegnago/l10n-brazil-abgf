ef82555d45f6db603feb91b495191900fd8e301d
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-10-24 14:17:23 -0300

    [ADD] Nova situação no intermediário do S-2300

--- sped_esocial/models/intermediarios/s2300_inicio_trabalhador_sem_vinculo_de_emprego.py
+++ sped_esocial/models/intermediarios/s2300_inicio_trabalhador_sem_vinculo_de_emprego.py
@@ -43,6 +43,7 @@ class SpedEsocialHrContrato(models.Model, SpedRegistroIntermediario):
             ('3', 'Erro(s)'),
             ('4', 'Sucesso'),
             ('5', 'Precisa Retificar'),
+            ('6', 'Retificado'),
         ],
         compute="compute_situacao_esocial",
         store=True,
